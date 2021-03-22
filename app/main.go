8b43335a30617d3ac0cac050005f2934b28ede39
Mathieu <mathieux51@gmail.com>
2021-03-22 13:41:22 +0100

    Add catchAllHandler

--- app/main.go
+++ app/main.go
@@ -7,13 +7,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func catchAllHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handler")
 	w.Write([]byte("Hello World\n"))
 }
 
 func main() {
 	log.Println("main")
 	r := mux.NewRouter()
-	r.HandleFunc("*", Handler)
+	r.PathPrefix("/").Handler(http.HandlerFunc(catchAllHandler))
 	log.Fatal(http.ListenAndServe(":10003", r))
 }
